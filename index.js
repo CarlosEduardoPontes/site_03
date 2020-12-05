28796d0dec18f3fec84ba1ad09a9978d8a5e8b97
Carlos Eduardo <ceduardo.pontes@hotmail.com>
2020-12-05 17:53:03 -0300

    Site em HTML, CSS e JS com bars e botoes animados em CSS

---
+++ index.js
@@ -0,0 +1,6 @@
+function carregar ()
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('foto')
+var date = new Date()
+var hora = data.getElementById()
+msg.inerhtml = 'Agora são $(hora) horas';
